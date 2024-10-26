cf797250b52e5961fbb71e5037353acfb5cae1d2
Vidyasagar Sadhu <srikanthvidyasagar.sadhu@sri.com>
2024-10-25 17:09:26 -0700

    minor changes

--- plot_results.py
+++ plot_results.py
@@ -17,7 +17,7 @@ if __name__ == "__main__":
 
     # add PROGRAM level args
     # parser.add_argument("--N_samples", type=int, default=256 * 10)
-    parser.add_argument("--data_dir", type=str, default="data/240712_Experimenal_Data/varying_all_noise", help="path to the directory containing training files")
+    parser.add_argument("--data_dir", type=str, default="data/240712_Experimental_Data/varying_all_noise", help="path to the directory containing training files")
     parser.add_argument("--base_folder", type=str, default="train", help="path to the root training folder where pla no_pla and bm train results are stored")
     parser.add_argument("--debug", action='store_true', help='adds --debug flag to runs')
     args = parser.parse_args()
